a1c9f3fa09c656031bc2da87368376a3b1aa43c7
Giovanni De Florio s292457@studenti.polito.it <giovannidf96@gmail.com>
2022-11-05 09:23:12 +0100

    added select_device

--- examples/capturingdefault.rs
+++ examples/capturingdefault.rs
@@ -1,8 +1,11 @@
-use Network_analyzer::init_default;
+use Network_analyzer::{select_default, print_packets, select_device};
 
 fn main() {
-    let mut cap = init_default();
+   /* let mut cap = init_default();
     while let Ok(packet) = cap.next_packet() {
         println!("received packet! {:?}", packet);
-    }
+    }*/
+
+    let mut cap = select_device();
+    print_packets(cap);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+use std::io;
 use pcap::{Active, Capture, Device};
 
-pub fn init_default() -> Capture<Active> {
+pub fn select_default() -> Capture<Active> {
 
     let main_device = Device::lookup().unwrap().unwrap();
     let mut cap = Capture::from_device(main_device).unwrap()
@@ -11,6 +12,40 @@ pub fn init_default() -> Capture<Active> {
     return cap;
 }
 
+pub fn select_device() -> Capture<Active> {
+
+    let mut i=0;
+        // list all of the devices pcap tells us are available
+    let dev_list= Device::list().expect("device lookup failed");
+        for device in &dev_list {
+            i+=1;
+            println!("{})  {:?}",i, device.desc.as_ref().unwrap());
+        }
+
+    let mut input_line = String::new();
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+    let mut number: usize = input_line.trim().parse().expect("Input not an integer");
+    number-=1;
+
+    let device = dev_list[number].clone();
+
+    println!("Selected {:?}",device.desc.as_ref().unwrap());
+
+    let mut cap = Capture::from_device(device).unwrap()
+        .promisc(true)
+        .snaplen(5000)
+        .open().unwrap();
+
+    return cap;
+}
+
+pub fn print_packets(mut cap:Capture<Active>){
+    while let Ok(packet) = cap.next_packet() {
+        println!("received packet! {:?}", packet);
+    }
+}
 
 
 
