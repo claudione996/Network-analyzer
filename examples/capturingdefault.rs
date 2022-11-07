c643345af5f7af39bf05e2bc08bbf8d1c5d01460
Giovanni De Florio s292457@studenti.polito.it <giovannidf96@gmail.com>
2022-11-07 23:20:31 +0100

    method resume (SocketListener)

--- examples/capturingdefault.rs
+++ examples/capturingdefault.rs
@@ -5,6 +5,10 @@ fn main() {
     let device = "\\Device\\NPF_{DFADCF5E-E518-4EB5-A225-3126223CB9A2}";
     let sock=SocketListener::new(device);
     std::thread::sleep(std::time::Duration::from_secs(5));
-    println!("sleep end");
+    println!("pause");
+    sock.pause();
+    println!("resume");
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    println!("pause");
     sock.pause();
 }
--- src/network_analyzer_components/socketlistener.rs
+++ src/network_analyzer_components/socketlistener.rs
@@ -1,18 +1,24 @@
 use std::collections::HashMap;
+use std::sync::mpsc::Sender;
 use crate::network_analyzer_components::aggregator::Aggregator;
+use crate::network_analyzer_components::ParsedPacket::ParsedPacket;
 use crate::network_analyzer_components::parser::Parser;
 
 pub struct SocketListener{
     parser: Parser,
-    aggregator:Aggregator
+    aggregator:Aggregator,
+    device:String,
+    aggregator_tx:Sender<ParsedPacket>
+
 }
 
 impl SocketListener {
-    pub fn new(device:&str) -> Self {
-        let agg=Aggregator::new();
-        let aggregator_tx=agg.get_sender();
-        let parser=Parser::new(device, aggregator_tx);
-        SocketListener{parser,aggregator:agg}
+    pub fn new(device_str:&str) -> Self {
+        let aggregator=Aggregator::new();
+        let aggregator_tx=aggregator.get_sender();
+        let parser=Parser::new(device_str, aggregator_tx.clone());
+        let device=String::from(device_str);
+        SocketListener{parser,aggregator,device,aggregator_tx}
     }
 
     pub fn pause(&self){
@@ -20,5 +26,9 @@ impl SocketListener {
         let data=self.aggregator.get_aggregated_data();
         let data=data.lock().unwrap();
         println!("{:?}",data);
+    }
+
+    pub fn resume(&mut self){
+        self.parser=Parser::new(self.device.as_str(), self.aggregator_tx.clone());
     }
 }
