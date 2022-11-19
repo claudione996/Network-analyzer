8f76767725bbe1a6e7427882d7e0dc5667ff763a
Alessio Bincoletto <s234970@studenti.polito.it>
2022-11-19 15:54:38 +0100

    ⚰️removed deprecated commented code

--- src/modules/parser.rs
+++ src/modules/parser.rs
@@ -31,10 +31,7 @@ impl Parser{
                     match cap.next_packet() {
                         Ok(packet) => {
                             let mut stopped =stopped.lock().unwrap();
-                           // println!("t1");
                             let stopped = cv.wait_while(stopped, |x| *x).unwrap();
-                           // println!("t2");
-                            //if *stopped {println!("parser stopped");break}
                             let p=parse_packet(packet);
                             match p {
                                 None => println!("packet not valid for parsing (neither IP/TCP, IP/UDP or IP/ICMP)"),
@@ -48,16 +45,12 @@ impl Parser{
     }
 
     pub fn stop_iter_cap(&self){
-      //  println!("stop1");
         let mut stopped =self.stopped.lock().unwrap();
-      //  println!("stop2");
         *stopped=true;
     }
 
     pub fn resume_iter_cap(&self){
-     //   println!("resume1");
         let mut stopped =self.stopped.lock().unwrap();
-     //   println!("resume2");
         *stopped=false;
         self.cv.notify_one();
     }
