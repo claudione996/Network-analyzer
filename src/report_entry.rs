488b820a18e94a130fa116736b8758de2ebb79df
Alessio Bincoletto <s234970@studenti.polito.it>
2022-11-25 19:14:47 +0100

    🔨 implemented Display trait for Connection and ConnectionMetadata

--- src/report_entry.rs
+++ src/report_entry.rs
@@ -1,3 +1,6 @@
+use core::fmt;
+use std::fmt::Formatter;
+
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Connection {
     pub source_ip: String,
@@ -25,10 +28,26 @@ impl Connection {
     }
 }
 
+impl fmt::Display for Connection {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let port_src = match self.source_port {
+            Some(x) => x.to_string(),
+            None => String::from("-"),
+        };
+        let port_dst = match self.destination_port {
+            Some(x) => x.to_string(),
+            None => String::from("-"),
+        };
+        write!(f,"| {0:<15} \t| {1:<15} \t| {2:<5} \t | {3:<5} \t| {4:<7} \t|", self.source_ip, self.destination_ip, port_src, port_dst, self.protocol)
+    }
+}
+
+
+#[derive(Debug)]
 pub struct ConnectionMetadata {
-    pub size: usize,
-    pub first_timestamp: String,
-    pub last_timestamp: String,
+pub size: usize,
+pub first_timestamp: String,
+pub last_timestamp: String,
 }
 
 impl ConnectionMetadata {
@@ -40,3 +59,10 @@ impl ConnectionMetadata {
         }
     }
 }
+
+impl fmt::Display for ConnectionMetadata {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f," {0:<9} \t| {1:<15} \t| {2:<3}|",self.size,self.first_timestamp,self.last_timestamp)
+    }
+}
+
