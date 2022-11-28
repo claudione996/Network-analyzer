8a27b71c154e12596f21a02c8929f35a7472aea9
Alessio Bincoletto <s234970@studenti.polito.it>
2022-11-28 19:19:54 +0100

    📝 added rustdoc to Connection and ConnectionMetadata structs

--- src/report_entry.rs
+++ src/report_entry.rs
@@ -1,6 +1,10 @@
 use core::fmt;
 use std::fmt::Formatter;
 
+/// This struct represents a directional connection between two hosts.
+/// It is used as a key in the aggregated data map.
+/// It represents source and destination hosts through their IP addresses and ports and specifies the type of connection through the `protocol` attribute.
+/// The source IP address and the source port are Option to allow representation of ICMP communications that are not associated to ports.
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Connection {
     pub source_ip: String,
@@ -11,6 +15,15 @@ pub struct Connection {
 }
 
 impl Connection {
+    /// Creates a new Connection struct.
+    /// # Arguments
+    /// * `source_ip` - The source IP address.
+    /// * `destination_ip` - The destination IP address.
+    /// * `source_port` - The source port.
+    /// * `destination_port` - The destination port.
+    /// * `protocol` - The protocol.
+    /// # Returns
+    /// A new Connection struct.
     pub fn new(
         source_ip: String,
         destination_ip: String,
@@ -28,6 +41,8 @@ impl Connection {
     }
 }
 
+/// Display implementation for the Connection struct.
+/// It is used to print the Connection struct as a markdown table entry.
 impl fmt::Display for Connection {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let port_src = match self.source_port {
@@ -42,7 +57,9 @@ impl fmt::Display for Connection {
     }
 }
 
-
+/// This struct represents aggregated data collected about a connection.
+/// It is used as a value in the aggregated data map.
+/// It contains the number of bytes exchanged between the two hosts and the timestamp of the first and last packet exchanged.
 #[derive(Debug)]
 pub struct ConnectionMetadata {
 pub size: usize,
@@ -51,6 +68,13 @@ pub last_timestamp: String,
 }
 
 impl ConnectionMetadata {
+    /// Creates a new ConnectionMetadata struct.
+    /// # Arguments
+    /// * `size` - The number of bytes exchanged.
+    /// * `first_timestamp` - The timestamp of the first packet exchanged.
+    /// * `last_timestamp` - The timestamp of the last packet exchanged.
+    /// # Returns
+    /// A new ConnectionMetadata struct.
     pub fn new(size: usize, first_timestamp: String, last_timestamp: String) -> Self {
         ConnectionMetadata {
             size,
